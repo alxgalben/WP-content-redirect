150c056282cf27c85d925052433f720e85bf391e
alxgalben <galbenalex2@gmail.com>
2024-12-19 13:08:46 +0200

    mod

--- wp-content/plugins/redirect-plugin/redirect-plugin.php
+++ wp-content/plugins/redirect-plugin/redirect-plugin.php
@@ -1,9 +1,13 @@
 <?php
 /*
-Plugin Name: redirect root
-Description: redirect
+Plugin Name: Redirect Plugin
+Plugin URI: https://example.com
+Description: A plugin to handle 301 redirects for old URLs to new ones.
 Version: 1.0
 Author: Alex Galben
+Author URI: https://example.com
+License: GPL2
+Text Domain: redirect-plugin
 */
 
 // prevenire acces direct
